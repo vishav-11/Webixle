1bfbd4601140062a780ed4c02d0ffdb62d63afca
Vishav Mahala <vishavmahala005@gmail.com>
2026-07-15 12:52:58 +0530

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -16,7 +16,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
-      url: "https://webixle.com/portfolio",
+      url: "https://webixle.com/demo",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
